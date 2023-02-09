14546f2b1fa1e3dd9b76586c525ea961ffeacc36
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-02-09 19:30:09 +0200

    add SessionServlet

---
+++ src/edu/AnastasiiaTkachuk/servlet/SessionServlet.java
@@ -0,0 +1,19 @@
+package edu.AnastasiiaTkachuk.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/sessions")
+public class SessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        System.out.println(session.isNew());
+    }
+}
